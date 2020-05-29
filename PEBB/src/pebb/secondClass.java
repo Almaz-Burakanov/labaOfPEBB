cafb5d281e4534571d641f74346d8e890ea5247b
Almaz-Burakanov <almazbs@mail.ru>
2020-05-29 15:31:30 +0500

    Добавлен вывод информации о GitHub

--- PEBB/src/pebb/secondClass.java
+++ PEBB/src/pebb/secondClass.java
@@ -4,6 +4,6 @@ import javax.swing.JOptionPane;
 
 public class secondClass {
 	public static void Information() {
-		JOptionPane.showMessageDialog(null, "Курсовая работа\n 7 вариант","Информация",JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null, "GitHub — крупнейший веб-сервис для хостинга IT-проектов и их совместной разработки. \nВеб-сервис основан на системе контроля версий Git и разработан на Ruby on Rails и Erlang компанией GitHub, Inc (ранее Logical Awesome). \nСервис бесплатен для проектов с открытым исходным кодом и (с 2019 года) небольших частных проектов, предоставляя им все возможности (включая SSL), \nа для крупных корпоративных проектов предлагаются различные платные тарифные планы.","Информация",JOptionPane.INFORMATION_MESSAGE);
 	}
 }
